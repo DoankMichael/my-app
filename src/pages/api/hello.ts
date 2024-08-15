7d5608235283df347fc3240aa3ab3303032a7934
DoankMichael <doankmichael79@gmail.com>
2024-08-15 23:23:52 +0700

    Pages & Layouts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,5 +7,5 @@ type Data = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: "Michael", age: "25" });
+  res.status(200).json({ name: "Michael", age: 25 });
 }
